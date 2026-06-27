e749b9e5abdd23c7ff62034d5ad35bc5cd802942
Charles <rhcharles0@gmail.com>
2026-06-28 02:32:37 +0900

    feat: require authentication for user signup

--- src/main/java/com/butingbe/domain/user/controller/UserController.java
+++ src/main/java/com/butingbe/domain/user/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.butingbe.domain.user.controller;
 
+import com.butingbe.domain.auth.security.AuthenticatedUser;
 import com.butingbe.domain.user.dto.request.SignUpReqDto;
 import com.butingbe.domain.user.dto.response.UserResDto;
 import com.butingbe.domain.user.service.UserService;
+import com.butingbe.global.error.exception.UnauthenticatedException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,7 +21,13 @@ public class UserController {
 
   /** 회원가입 API */
   @PostMapping("/signup")
-  public ResponseEntity<Void> signUp(@RequestBody @Valid SignUpReqDto request) {
+  public ResponseEntity<Void> signUp(
+      @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+      @RequestBody @Valid SignUpReqDto request) {
+    if (authenticatedUser == null) {
+      throw new UnauthenticatedException();
+    }
+
     userService.signUp(request);
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
