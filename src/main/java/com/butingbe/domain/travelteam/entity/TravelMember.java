76b7d1354a7316549a3df6c4c7d6370a38bc8fe9
DESKTOP-100KUGP\준연 <whwnsdus11@naver.com>
2026-07-03 01:08:38 +0900

    feat: 여행 멤버 매핑 테이블 작성  #62

---
+++ src/main/java/com/butingbe/domain/travelteam/entity/TravelMember.java
@@ -0,0 +1,30 @@
+package com.butingbe.domain.travelteam.entity;
+
+import com.butingbe.domain.temp.entity.TravelTemp;
+import com.butingbe.domain.user.entity.User;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+public class TravelMember {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "travel_id")
+    private TravelTemp travel;  // 임시 Travel 연결
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private TravelTeamRole role;
+
+
+}
