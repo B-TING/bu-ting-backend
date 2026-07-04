867abb24484458053b128a0969a43b6e125fe36d
DESKTOP-100KUGP\준연 <whwnsdus11@naver.com>
2026-07-04 17:45:48 +0900

    feat: 여행팀 초대 엔티티 작성 #65

---
+++ src/main/java/com/butingbe/domain/travelteam/entity/TravelInvite.java
@@ -0,0 +1,30 @@
+package com.butingbe.domain.travelteam.entity;
+
+import com.butingbe.domain.temp.entity.TravelTemp;
+import jakarta.persistence.*;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+public class TravelInvite {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "travel_id", nullable = false)
+    private TravelTemp travel;  // 임시 Travel 연결
+
+    @Column(nullable = false, unique = true)
+    private UUID token;
+
+    @Column(nullable = false)
+    private Boolean used = false;
+
+    @Column(nullable = false)
+    private OffsetDateTime createdAt;
+
+    @Column(nullable = false)
+    private OffsetDateTime expiredAt;
+}
